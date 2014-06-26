aa5e7bcad67bd02447750f0b94b906035bb47b35
Lucas Churchill <lucas@agtlucas.com>
2014-06-26 16:40:29 -0300

    Update Post Controller

--- evil/app/controllers/posts_controller.rb
+++ evil/app/controllers/posts_controller.rb
@@ -4,9 +4,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new(post_params)
     @post.save
     redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :text)
   end
 
 end
