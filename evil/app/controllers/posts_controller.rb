1c288d3b5c49a51b63e6bccbcd605c97ddf131cd
Lucas Churchill <lucas@agtlucas.com>
2014-06-27 10:27:28 -0300

    Update private method on post controller

--- evil/app/controllers/posts_controller.rb
+++ evil/app/controllers/posts_controller.rb
@@ -9,13 +9,13 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :text)
     end
-
-  def show
-    @post = Post.find(params[:id])
-  end
 
 end
