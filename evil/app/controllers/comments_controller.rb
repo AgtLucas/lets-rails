9ee8dd9c5bef51b32132d241a37b8748b7bcb632
Lucas Churchill <lucas@agtlucas.com>
2014-06-28 15:25:15 -0300

    Adding create method on comments controller

--- evil/app/controllers/comments_controller.rb
+++ evil/app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+
+    redirect_to post_path(@post)
+  end
+
 end
